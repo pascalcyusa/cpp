cc58a8e10f4628f4415275a0c987da135a61b3bb
Jean Pascal Cyusa Shyaka <shyakaadolf@gmail.com>
2022-04-22 17:50:28 +0200

    commit

--- sec-11/howFunctionCallsWork/howFunctionCallsWork/main.cpp
+++ sec-11/howFunctionCallsWork/howFunctionCallsWork/main.cpp
@@ -9,4 +9,28 @@
 
 using namespace std;
 
+void funct2(int &x, int y, int z) {
+    x += y+z;
+}
+
+int funct1(int a, int b) {
+    int result {};
+    result = a+b;
+    funct2(result, a, b);
+    return result;
+}
+
+int main() {
+    int x {10};
+    int y {20};
+    int z {};
+    z = funct1(x, y);
+    cout << z << endl;
+    
+    
+    
+    
+    cout << endl;
+    return 0;
+}
 
