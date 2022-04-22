9e943e8d5519e3326cdc65608e3032159710ee28
Jean Pascal Cyusa Shyaka <shyakaadolf@gmail.com>
2022-04-22 17:17:42 +0200

    edit on rand project

--- sec-11/rand/rand/main.cpp
+++ sec-11/rand/rand/main.cpp
@@ -11,19 +11,48 @@
 
 using namespace std;
 
-int main() {
+void randomNumber1();
+void randomNumber2();
+
+void randomNumber1() {
     int randomNumber {};
-    size_t count {10};
+    size_t count {6};
     int min {1};
-    int max {6};
-    
+    int max {49};
+
     cout << "RAND_MAX on my system is " << RAND_MAX << endl;
-    srand(time(nullptr));
-    
+
+    srand(static_cast<unsigned int>(time(nullptr)));  // seed random number generator
+
     for (size_t i {1}; i<=count; i++) {
         randomNumber = rand()%max + min;
         cout << randomNumber << endl;
     }
+}
+
+void randomNumber2() {
+    srand(static_cast<unsigned int>(time(nullptr)));
+
+    for (int count=1; count <= 100; ++count) {
+        cout << rand() << "\t";
+
+        // display 5 random numbers per row
+        if (count % 5 == 0)
+            cout << "\n";
+        }
+}
+
+
+int main() {
+    cout << "\n\n\t\u26FA INZOZI LOTTO RANDOM NUMBERS GENERATOR \u26E9\n\n";
     
+    randomNumber1();
+    cout << "\n\n------------------------- OTROS -----------------------\n\n";
+    randomNumber2();
+
+
+    cout << endl << endl;
     return 0;
 }
+
+
