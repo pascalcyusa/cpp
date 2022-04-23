24c3e3740a6c065d7a39cd7dc69f3c6768453c03
Jean Pascal Cyusa Shyaka <shyakaadolf@gmail.com>
2022-04-23 12:25:33 +0200

    recursion
    fibonacci series;
    factorial

--- sec-11/factorial/factorial/main.cpp
+++ sec-11/factorial/factorial/main.cpp
@@ -9,11 +9,21 @@
 
 using namespace std;
 
+unsigned long long factorial(unsigned long long n);
+
+unsigned long long factorial(unsigned long long n) {
+    if (n==0)
+        return 1;
+    return n * factorial(n-1);
+}
+
 int main() {
+    cout << "factorial of 3 is " << factorial(3) << endl;
+    cout << "factorial of 5 is " << factorial(5) << endl;
+    cout << "factorial of 8 is " << factorial(8) << endl;
+    cout << "factorial of 10 is " << factorial(10) << endl;
+    cout << "factorial of 10 is " << factorial(20) << endl;
 
-    
-
-    
     cout << endl;
     return 0;
 }
--- sec-11/fibonacci/fibonacci/main.cpp
+++ sec-11/fibonacci/fibonacci/main.cpp
@@ -9,10 +9,20 @@
 
 using namespace std;
 
+unsigned long long fibonacci(unsigned long long n);
+
+unsigned long long fibonacci(unsigned long long n) {
+    if (n<=1)
+        return n;
+    return fibonacci(n-1) + fibonacci(n-2); // recursion
+}
+
+
 int main() {
-
-    
-
+    cout << fibonacci(5) << endl;
+    cout << fibonacci(6) << endl;
+    cout << fibonacci(30) << endl;
+    cout << fibonacci(40) << endl;
     
     cout << endl;
     return 0;
