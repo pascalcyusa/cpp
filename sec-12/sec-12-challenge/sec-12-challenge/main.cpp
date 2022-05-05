5415d5cde3c627a7f428d316f8dbc8170275ff3d
Jean Pascal Cyusa Shyaka <shyakaadolf@gmail.com>
2022-05-05 22:37:37 +0200

    added sec-12 challenge

---
+++ sec-12/sec-12-challenge/sec-12-challenge/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  sec-12-challenge
+//
+//  Created by Jean Pascal Cyusa Shyaka on 5/4/22.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    
+    cout << endl;
+    return 0;
+}
