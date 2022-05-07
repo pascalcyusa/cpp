0624cd9a8d30b4e642294eeb8dbc579c5fa42dbc
Jean Pascal Cyusa Shyaka <shyakaadolf@gmail.com>
2022-05-07 21:36:38 +0200

    sec-12-challenge

--- sec-12/sec-12-challenge/sec-12-challenge/main.cpp
+++ sec-12/sec-12-challenge/sec-12-challenge/main.cpp
@@ -9,6 +9,31 @@
 
 using namespace std;
 
+int *apply_all(const int *const arr1, size_t size1, const int *const arr2, size_t size2) {
+    int *new_array {};
+    
+    new_array = new int[size1 * size2];
+    
+    int position {0};
+    for (size_t i{0}; i<size2; i++) {
+        for (size_t j{0}; j<size1; j++) {
+            new_array[position] = arr1[j] * arr2[i];
+            ++position;
+        }
+    }
+    return new_array;
+}
+
+
+void print(const int *const arr, size_t size) {
+    cout << "[ ";
+    for (size_t i{0}; i<size; i++)
+        cout << arr[i] << " ";
+    cout << " ]";
+    cout << endl;
+}
+
+
 int main() {
     const size_t array1_size {5};
     const size_t array2_size {3};
@@ -31,3 +56,5 @@ int main() {
     cout << endl;
     return 0;
 }
+
+
