00cbab0c54db18efb62e445cf24187a45be52768
Jean Pascal Cyusa Shyaka <shyakaadolf@gmail.com>
2023-01-14 17:18:38 -0500

    commit!!

--- sec-6/var/var/main.cpp
+++ sec-6/var/var/main.cpp
@@ -6,7 +6,7 @@
 //
 
 #include <iostream>
-#include <climits>
+#include <climits> // for using sizeof
 
 
 using namespace std;
@@ -14,32 +14,29 @@ using namespace std;
 int main() {
     // Prints the size of different data types
     
-    cout << "**** SIZE OF INTEGER TYPES *****" << endl;
+    cout << "\n\t**** SIZE OF INTEGER TYPES *****" << endl;
     
-    cout << "The size of int is: " << sizeof(int) << '\x20' << "Bytes" << endl;
-    cout << "The size of unsigned int is: " << sizeof(unsigned int) << '\x20' << "Bytes" << endl;
-    cout << "The size of short is: " << sizeof(short) << '\x20' << "Bytes" << endl;
-    cout << "The size of unsigned short is: " << sizeof(unsigned short) << '\x20' << "Bytes" << endl << '\r';
+    cout << "The size of int is: " << sizeof(int) << " bytes" << endl;
+    cout << "The size of unsigned int is: " << sizeof(unsigned int) << " bytes" << endl;
+    cout << "The size of short is: " << sizeof(short) << " bytes" << endl;
+    cout << "The size of unsigned short is: " << sizeof(unsigned short) << " bytes" << endl << '\r';
     
-    // Minimum integers that can be stored
+    // Minimum & maximum integers that can be stored
     
-    cout << "**** MIN AND MAX *****" << endl;
+    cout << "\n\t**** MIN AND MAX *****" << endl;
     
-    cout << "Minimum short is: " << SHRT_MIN << endl;
-    cout << "Minimum int is: " << INT_MIN << endl;
-    cout << "Minimum char is: " << CHAR_MIN << endl;
-    cout << "Minimum long long is: " << LLONG_MIN << endl << '\r';
-    
-    cout << "Maximum short is: " << SHRT_MAX << endl;
-    cout << "Maximum int is: " << INT_MAX << endl;
-    cout << "Maximum char is: " << CHAR_MAX << endl;
-    cout << "Maximum long long is: " << LLONG_MAX << endl << '\r';
+    cout << "Minimum short is: " << SHRT_MIN << " and Maximum short is: " << SHRT_MAX << endl;
+    cout << "Minimum int is: " << INT_MIN << " and Maximum int is: " << INT_MAX << endl;
+    cout << "Minimum char is: " << CHAR_MIN << " and Maximum char is: " << CHAR_MAX << endl;
+    cout << "Minimum long is: " << LONG_MIN << " and Maximum long is: " << LONG_MAX << endl;
+    cout << "Minimum long long is: " << LLONG_MIN << " and Maximum long long is: " << LLONG_MAX << endl;
     
     // Using variables
     
-    cout << "**** SIZE OF VARIBLES ****" << endl;
+    cout << "\n\t**** SIZE OF VARIBLES ****" << endl;
     
     int age {213223};
-    cout << "Size of integer age is: " << sizeof(age) << '\x20' << "Bytes" << endl << endl;
-
+    cout << "Size of age is: " << sizeof(age) << '\x20' << "Bytes"
+    << endl << endl;
+    return 0;
 }
